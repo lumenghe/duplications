6fd9678c276b830e75209fdffea7aa819ed13d5a
lumenghe <lumenghe@gmail.com>
2021-03-24 07:17:56 +0100

    feat(process): add process function

--- app_pandas.py
+++ app_pandas.py
@@ -19,6 +19,40 @@ class DuplicationsFinder:
         df = pd.read_csv(filepath, sep="\t")
         return df
 
+    def process(self, df, output):
+        """process to find duplications and save in output file
+        :param df: dataframe read from file
+        :param output: output file path
+        :return: None
+        """
+        df = df.sort_values(["year", "length"])
+
+        # Get all unique years
+        years = sorted(set(df["year"]))
+
+        # Get all unique genre
+        genres = set(df["genre"])
+        genres = sorted(set.union(*(set(s.split(",")) for s in genres)))
+
+        # Add one-hot encoding for all genres + \N
+        for genre in genres:
+            if genre != "\\N":
+                col = df["genre"].str.contains(genre).values
+            else:
+                col = df["genre"] == "\\N"
+
+            df.insert(0, genre, col)
+            logger.info(f"added column {genre}.")
+        # Add +/- %5 lengths
+        df.insert(0, "min_length", self.LEFT_THRESHOLD * df["length"])
+        df.insert(0, "max_length", self.RIGHT_THRESHOLD * df["length"])
+
+        matchings = self.process_per_year(df, years, genres)
+
+        final_matchings = self.post_process(matchings)
+
+        # save duplications per year
+        self.save_duplicates(final_matchings, output)
 def main():
     ...
 
